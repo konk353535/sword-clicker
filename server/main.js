c3cc8af52ea368ac3d418310547a7762de01e9e2
Jason Romaior <jasonromaior@Jasons-MacBook-Pro.local>
2017-04-06 20:42:26 +1000

    Indexes to improve mining update performance

--- server/main.js
+++ server/main.js
@@ -3,8 +3,12 @@ import '/imports/startup/both';
 import '/imports/startup/server';
 
 import { resumeBattle } from '/server/api/battles/battles';
+
 import { Battles } from '/imports/api/battles/battles';
 import { Crafting } from '/imports/api/crafting/crafting';
+import { Items } from '/imports/api/items/items';
+import { Mining, MiningSpace } from '/imports/api/mining/mining';
+import { Skills } from '/imports/api/skills/skills';
 
 Meteor.startup(() => {
   // Start processing abandoned battles
@@ -18,4 +22,9 @@ Meteor.startup(() => {
 
   // Ensure indexes on key databases
   Crafting._ensureIndex({ owner: 1 });
+  Skills._ensureIndex({ owner: 1 });
+  Items._ensureIndex({ owner: 1 });
+  Mining._ensureIndex({ owner: 1 });
+  MiningSpace._ensureIndex({ owner: 1 });
+
 });

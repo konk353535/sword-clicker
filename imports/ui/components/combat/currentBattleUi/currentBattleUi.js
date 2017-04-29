257c6c1d3bfcdd1ac1305d1fe9728f72e11cd764
Jason Romaior <jasonromaior@Jasons-MacBook-Pro.local>
2017-04-29 22:07:29 +1000

    Potential client bug fixes

--- imports/ui/components/combat/currentBattleUi/currentBattleUi.js
+++ imports/ui/components/combat/currentBattleUi/currentBattleUi.js
@@ -15,6 +15,11 @@ Template.currentBattleUi.onCreated(function bodyOnCreated() {
   this.autorun(() => {
     const currentBattle = Battles.findOne({ finished: false });
 
+    const myUnit = _.findWhere(currentBattle.units, { id: Meteor.userId() });
+    if (myUnit) {
+      this.state.set('myUnit', myUnit);
+    }
+
     if (currentBattle) {
       if (currentBattle.tickEvents.length > 3) {
         // Only show user owned ticks
@@ -24,32 +29,34 @@ Template.currentBattleUi.onCreated(function bodyOnCreated() {
       }
       currentBattle.tickEvents.forEach((tickEvent, tickEventIndex) => {
         const offset = $(`#${tickEvent.to}`).offset();
-        let color;
-        if (tickEvent.label == 0) {
-          color = 'blue';
-        } else {
-          color = 'red';
+        if (offset) {
+          let color;
+          if (tickEvent.label == 0) {
+            color = 'blue';
+          } else {
+            color = 'red';
+          }
+
+          // Determine left based on tick # + tickEventIndex
+          offset.left += -20 + ((tickEventIndex % 3) * 45); // -10 to 50
+
+          // Attempt to push floating text down when more then 3
+          if (tickEventIndex % 6 >= 3) {
+            offset.top += 40;
+          }
+
+          let element = `
+            <p
+              class='floating-text'
+              data-count=1
+              style='top: ${offset.top}px; left: ${offset.left}px; opacity: 1.0; color: ${color}'>
+              <i class="lilIcon-attack"></i>
+              ${tickEvent.label}
+            </p>
+          `;
+
+          $('body').append(element);
         }
-
-        // Determine left based on tick # + tickEventIndex
-        offset.left += -20 + ((tickEventIndex % 3) * 45); // -10 to 50
-
-        // Attempt to push floating text down when more then 3
-        if (tickEventIndex % 6 >= 3) {
-          offset.top += 40;
-        }
-
-        let element = `
-          <p
-            class='floating-text'
-            data-count=1
-            style='top: ${offset.top}px; left: ${offset.left}px; opacity: 1.0; color: ${color}'>
-            <i class="lilIcon-attack"></i>
-            ${tickEvent.label}
-          </p>
-        `;
-
-        $('body').append(element);
       });
     }
   })
@@ -63,11 +70,6 @@ Template.currentBattleUi.helpers({
 
     if (!currentBattle) {
       return {};
-    }
-
-    const myUnit = _.findWhere(currentBattle.units, { id: Meteor.userId() });
-    if (myUnit) {
-      Template.instance().state.set('myUnit', myUnit);
     }
 
     return currentBattle;

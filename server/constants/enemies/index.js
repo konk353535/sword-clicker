e4e72e7f98c7cad7e366d0494965147720b69d69
Jason Romaior <jasonromaior@Jasons-MacBook-Pro.local>
2017-05-22 20:30:27 +1000

    Starting work on custom mobs

--- server/constants/enemies/index.js
+++ server/constants/enemies/index.js
@@ -12,51 +12,78 @@ export const ENEMIES = Object.assign({
   grasshopper: {
     id: 'grasshopper',
     icon: 'grasshopper',
-    name: 'grasshopper'
+    name: 'grasshopper',
+    statBuffs: [{
+      type: 'plus',
+      key: 'defense',
+      amount: 10
+    }, {
+      type: 'times',
+      key: 'armor',
+      amount: 0.5
+    }],
+    swarmRange: [1, 2]
   },
 
   fly: {
     id: 'fly',
     icon: 'fly',
-    name: 'fly'
+    name: 'fly',
+    statBuffs: [{
+      type: 'plus',
+      key: 'defense',
+      amount: 15
+    }, {
+      type: 'times',
+      key: 'health',
+      amount: 0.5
+    }],
+    swarmRange: [3, 6]
   },
 
+  // Steals ur stats
   goblin: {
     id: 'goblin',
     icon: 'goblin',
     name: 'goblin'
   },
 
+  // Chance to activate evasive maneuvers
   young_ninja: {
     id: 'young_ninja',
     icon: 'youngNinja',
     name: 'young ninja'
   },
 
+  // When below 50% hp, goes berserk
   dwarf: {
     id: 'dwarf',
     icon: 'dwarf',
     name: 'dwarf'
   },
 
+  // Randomly casts spell, which reduces healing
   demon: {
     id: 'demon',
     icon: 'demon',
     name: 'demon'
   },
 
+  // Shield ability of kinda, every 30 seconds block 70% of damage for 25 seconds
   spartan: {
     id: 'spartan',
     icon: 'spartan',
     name: 'spartan'
   },
 
+  // Frost armor buff
   ice_giant: {
     id: 'ice_giant',
     icon: 'iceGiant',
     name: 'ice giant'
   },
 
+  // All spirits will be weak to magic, strong against armor
   cursed_spirit: {
     id: 'cursed_spirit',
     icon: 'cursedSpirit',
@@ -93,120 +120,199 @@ export const ENEMIES = Object.assign({
     name: 'mithril spirit'
   },
 
+  // Can spawn multiple farmers
   farmer: {
     id: 'farmer',
     icon: 'farmer',
-    name: 'farmer'
+    name: 'farmer',
+    swarmRange: [2, 4]
   },
 
+  // Cuts through armor
   beaver: {
     id: 'beaver',
     icon: 'beaver',
     name: 'beaver'
   },
 
+  // Has poisoned blades ability
   snake: {
     id: 'snake',
     icon: 'snake',
     name: 'snake'
   },
 
+  // Can spawn multiple worms
   worm: {
     id: 'worm',
     icon: 'worm',
-    name: 'worm'
+    name: 'worm',
+    swarmRange: [3, 5]
   },
 
   butterfly: {
     id: 'butterfly',
     icon: 'butterfly',
-    name: 'butterfly'
+    name: 'butterfly',
+    statBuffs: [{
+      type: 'plus',
+      key: 'defense',
+      amount: 5
+    }, {
+      type: 'times',
+      key: 'armor',
+      amount: 0.75
+    }, {
+      type: 'times',
+      key: 'magicArmor',
+      amount: 2
+    }]
   },
 
+  // Swarm
   dragonfly: {
     id: 'dragonfly',
     icon: 'dragonfly',
-    name: 'dragonfly'
+    name: 'dragonfly',
+    statBuffs: [{
+      type: 'times',
+      key: 'attackSpeed',
+      times: 3
+    }, {
+      type: 'times',
+      key: 'damageMax',
+      times: 0.4
+    }, {
+      type: 'times',
+      key: 'damage',
+      times: 0.4
+    }]
   },
 
+  // Crit Chance
   bee: {
     id: 'bee',
     icon: 'bee',
-    name: 'bee'
+    name: 'bee',
+    swarmRange: [1, 10]
   },
 
+  // Stun chance
   wasp: {
     id: 'wasp',
     icon: 'wasp',
     name: 'wasp'
   },
 
+  // Gains attack speed, the more damage taken
   angry_miner: {
     id: 'angry_miner',
     icon: 'angry_miner',
     name: 'angry miner'
   },
 
+  // Crit chance
   bird: {
     id: 'bird',
     icon: 'bird',
     name: 'bird'
   },
 
+  // Chance to bleed
   rat: {
     id: 'rat',
     icon: 'rat',
     name: 'rat'
   },
 
+  // Resistance to poison
   lizard: {
     id: 'lizard',
     icon: 'lizard',
     name: 'lizard'
   },
 
+  // Lots of health
   elephant: {
     id: 'elephant',
     icon: 'elephant',
-    name: 'elephant'
+    name: 'elephant',
+    statBuffs: [{
+      type: 'times',
+      key: 'health',
+      amount: 2
+    }, {
+      type: 'times',
+      key: 'damageMax',
+      amount: 3
+    }, {
+      type: 'times',
+      key: 'attackSpeed',
+      amount: 0.4
+    }]
   },
 
+  // Gains shit tons of armor, each attack reduces it by one
   crab: {
     id: 'crab',
     icon: 'crab',
     name: 'crab'
   },
 
+  // Spiked armor ability
   echidna: {
     id: 'echidna',
     icon: 'echidna',
     name: 'echidna'
   },
 
+  // Lots of warmor
   snail: {
     id: 'snail',
     icon: 'snail',
-    name: 'snail'
+    name: 'snail',
+    statBuffs: [{
+      type: 'times',
+      key: 'armor',
+      amount: 2
+    }, {
+      type: 'times',
+      key: 'health',
+      amount: 0.75
+    }]
   },
 
+  // Randomly burrows, healing for X health
   wombat: {
     id: 'wombat',
     icon: 'wombat',
     name: 'wombat'
   },
 
+  // Resistance to magic
   brown_mage: {
     id: 'brown_mage',
     icon: 'brown_mage',
-    name: 'brown mage'
+    name: 'brown mage',
+    statBuffs: [{
+      type: 'times',
+      key: 'magicArmor',
+      amount: 100
+    }, {
+      type: 'times',
+      key: 'armor',
+      amount: 0.2
+    }]
   },
 
+  // Casts earthen armor abilities
   earth_mage: {
     id: 'earth_mage',
     icon: 'earthMage',
     name: 'earth mage'
   },
 
+  // Casts fire abilities
   fire_mage: {
     id: 'fire_mage',
     icon: 'fireMage',
@@ -216,21 +322,34 @@ export const ENEMIES = Object.assign({
   blue_mage: {
     id: 'blue_mage',
     icon: 'blue_mage',
-    name: 'blue mage'
+    name: 'blue mage',
+    statBuffs: [{
+      type: 'times',
+      key: 'magicArmor',
+      amount: 100
+    }, {
+      type: 'times',
+      key: 'armor',
+      amount: 0.2
+    }]
   },
 
+  // Breeds, has a chance each second to spawn another rabbit
   rabbit: {
     id: 'rabbit',
     icon: 'rabbit',
     name: 'rabbit'
   },
 
+  // Venom + Swarm
   spider: {
     id: 'spider',
     icon: 'spider',
-    name: 'spider'
+    name: 'spider',
+    swarmRange: [1, 3]
   },
 
+  // Stronger venom, lower chance
   jellyFish: {
     id: 'jellyFish',
     icon: 'jellyFish',
--- server/constants/floors/generators/genericTower.js
+++ server/constants/floors/generators/genericTower.js
@@ -14,22 +14,48 @@ export const genericTowerMonsterGenerator = function(floor, room) {
     id: selectedMonster.id,
     icon: selectedMonster.icon,
     name: selectedMonster.name,
+    buffs: selectedMonster.buffs || [],
     stats: {
-      health: room * 25 * floor,
-      healthMax: room * 25 * floor,
-      attack: room * 3 * floor,
-      attackMax: room * 4 * floor,
+      health: room * 25 * floor * (1 + (floor / 3)),
+      healthMax: room * 25 * floor * (1 + (floor / 3)),
+      attack: room * 3 * floor * (1 + (floor / 3)),
+      attackMax: room * 4 * floor * (1 + (floor / 3)),
       attackSpeed: 0.5,
-      accuracy: room * 3 * floor,
+      accuracy: room * 3 * floor * (1 + (floor / 3)),
       armor: room * 30 * (floor / 4),
-      defense: room * 2 * floor,
-      magicArmor: room * 1.5 * floor,
+      defense: room * 2 * floor * (1 + (floor / 3)),
+      magicArmor: room * 1.5 * floor * (1 + (floor / 3)),
       damageTaken: 1
     },
     rewards: []
   }
 
+  if (selectedMonster.statBuffs) {
+    selectedMonster.statBuffs.forEach((statBuff) => {
+      if (statBuff.type === 'plus') {
+        monster.stats[statBuff.key] += statBuff.amount
+      } else if (statBuff.type === "times") {
+        monster.stats[statBuff.key] *= statBuff.amount        
+      }
+    })
+  }
+
   monster.stats.attackSpeedTicks = attackSpeedTicks(monster.stats.attackSpeed);
+
+  // Is this a swarm mob?
+  if (selectedMonster.swarmRange) {
+    const unitCount = _.random(selectedMonster.swarmRange[0], selectedMonster.swarmRange[1]);
+    // Divide monsters health
+    monster.stats.health /= unitCount; // Divide health evenly
+    monster.stats.health *= 1.3; // To account for aoe
+    monster.stats.healthMax = monster.stats.health;
+    const allMonsters = [];
+    for (let i = 0;i < unitCount; i++) {
+      allMonsters.push(JSON.parse(JSON.stringify(monster)));
+    }
+
+    return allMonsters;
+  }
 
   return [monster];
 }

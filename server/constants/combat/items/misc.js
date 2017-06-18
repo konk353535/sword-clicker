acbeb9351e5d31c60dfae35a50df07b30bd0f622
Jason Romaior <jasonromaior@Jasons-MacBook-Pro.local>
2017-06-18 15:08:58 +1000

    Buckler and kite shields

--- server/constants/combat/items/misc.js
+++ server/constants/combat/items/misc.js
@@ -93,6 +93,225 @@ export const MISC_ITEMS = {
     }]
   },
 
+  /* Kite Shield +70% armor, +15% defense, -Attack Speed, -Accuracy */
+  "bronze_kite_shield": {
+    "id": "bronze_kite_shield",
+    "icon": "bronzeKiteShield",
+    "category": "combat",
+    "weaponType": "shield",
+    "slot": "offHand",
+    "name": "bronze kite shield",
+    "sellPrice": 75,
+    "description": "Provides high defense bonuses",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 3.6,
+      "defense": 6.3,
+      "armor": 30,
+      "accuracy": -10,
+      "attackSpeed": -0.15
+    },
+    "extraStats": {
+      "healthMax": 1.1,
+      "defense": 2.0,
+      "armor": 9.3
+    }
+  },
+
+  "steel_kite_shield": {
+    "id": "steel_kite_shield",
+    "icon": "steelKiteShield",
+    "category": "combat",
+    "weaponType": "shield",
+    "slot": "offHand",
+    "name": "steel kite shield",
+    "sellPrice": 75,
+    "description": "Provides high defense bonuses",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 8.6,
+      "defense": 15.1,
+      "armor": 73,
+      "accuracy": -15,
+      "attackSpeed": -0.15
+    },
+    "extraStats": {
+      "healthMax": 2.6,
+      "defense": 4.6,
+      "armor": 22.3
+    }
+  },
+
+  "obsidian_kite_shield": {
+    "id": "obsidian_kite_shield",
+    "icon": "obsidianKiteShield",
+    "category": "combat",
+    "weaponType": "shield",
+    "slot": "offHand",
+    "name": "obsidian kite shield",
+    "sellPrice": 75,
+    "description": "Provides high defense bonuses",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 14.8,
+      "defense": 26.4,
+      "armor": 127.4,
+      "accuracy": -20,
+      "attackSpeed": -0.15
+    },
+    "extraStats": {
+      "healthMax": 4.4,
+      "defense": 7.9,
+      "armor": 38.4
+    }
+  },
+
+  "meteorite_kite_shield": {
+    "id": "meteorite_kite_shield",
+    "icon": "meteoriteKiteShield",
+    "category": "combat",
+    "weaponType": "shield",
+    "slot": "offHand",
+    "name": "meteorite kite shield",
+    "sellPrice": 75,
+    "description": "Provides large defense bonuses",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 24.4,
+      "defense": 43.1,
+      "armor": 210,
+      "accuracy": -30,
+      "attackSpeed": -0.15
+    },
+    "extraStats": {
+      "healthMax": 7.3,
+      "defense": 13.1,
+      "armor": 63.4
+    }
+  },
+
+  /* Buckler -30% armor, -10% defense, + small accuracy */
+
+  "tin_buckler": {
+    "id": "tin_buckler",
+    "icon": "tinBuckler",
+    "category": "combat",
+    "weaponType": "buckler",
+    "slot": "offHand",
+    "name": "tin buckler",
+    "sellPrice": 75,
+    "description": "Provides average defense bonuses",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 2.8,
+      "defense": 3.8,
+      "armor": 9.8,
+      "accuracy": 2
+    },
+    "extraStats": {
+      "healthMax": 0.8,
+      "defense": 1.3,
+      "armor": 3.3,
+      "accuracy": 1.2
+    }
+  },
+
+  "gold_buckler": {
+    "id": "gold_buckler",
+    "icon": "goldBuckler",
+    "category": "combat",
+    "weaponType": "buckler",
+    "slot": "offHand",
+    "name": "gold buckler",
+    "sellPrice": 75,
+    "description": "Provides average defense bonuses",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 6.5,
+      "defense": 9,
+      "armor": 23.1,
+      "accuracy": 4.5
+    },
+    "extraStats": {
+      "healthMax": 2,
+      "defense": 2.5,
+      "armor": 7.1,
+      "accuracy": 1.8
+    }
+  },
+
+  "tungsten_buckler": {
+    "id": "tungsten_buckler",
+    "icon": "tungstenBuckler",
+    "category": "combat",
+    "weaponType": "buckler",
+    "slot": "offHand",
+    "name": "tungsten buckler",
+    "sellPrice": 75,
+    "description": "Provides average defense bonuses",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 13.2,
+      "defense": 18.5,
+      "armor": 47.3,
+      "accuracy": 5.5
+    },
+    "extraStats": {
+      "healthMax": 4,
+      "defense": 5.4,
+      "armor": 14,
+      "accuracy": 2.4
+    }
+  },
+
+  "mithril_buckler": {
+    "id": "mithril_buckler",
+    "icon": "mithrilBuckler",
+    "category": "combat",
+    "weaponType": "buckler",
+    "slot": "offHand",
+    "name": "mithril buckler",
+    "sellPrice": 75,
+    "description": "Provides average defense bonuses",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 18.4,
+      "defense": 25.6,
+      "armor": 65.3,
+      "accuracy": 6.6
+    },
+    "extraStats": {
+      "healthMax": 5.5,
+      "defense": 6.6,
+      "armor": 19.6,
+      "accuracy": 2.8
+    }
+  },
+
+  "elven_steel_buckler": {
+    "id": "elven_steel_buckler",
+    "icon": "elvenSteelBuckler",
+    "category": "combat",
+    "weaponType": "buckler",
+    "slot": "offHand",
+    "name": "elven steel buckler",
+    "sellPrice": 75,
+    "description": "Provides average defense bonuses",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 30,
+      "defense": 42.9,
+      "armor": 107.8,
+      "accuracy": 8.9
+    },
+    "extraStats": {
+      "healthMax": 9,
+      "defense": 12.1,
+      "armor": 32.1,
+      "accuracy": 3.6
+    }
+  }
+
   spirit_shield: {
     id: 'spirit_shield',
     icon: 'spiritShield',

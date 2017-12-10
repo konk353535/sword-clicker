198b7dfc2bc951d22bf199641f0bfd6c798ba546
Nick Breslin <nickbreslin@gmail.com>
2017-12-09 23:55:13 -0800

    Floor 12-20 drops

--- server/constants/floors/tower/floor12/index.js
+++ server/constants/floors/tower/floor12/index.js
@@ -50,6 +50,13 @@ export const TOWER_FLOOR_12 = {
         { type: 'item', itemId: 'obsidian_hammer', amount: 1 },
         { type: 'item', itemId: 'obsidian_wand', amount: 1 }
       ]
+    }, {
+      chance: 1 / 48,
+      rewards: [
+        { type: 'item', itemId: 'amber_wizard_hat', amount: 1 },
+        { type: 'item', itemId: 'amber_wizard_shirt', amount: 1 },
+        { type: 'item', itemId: 'amber_wizard_shorts', amount: 1 }
+      ]
     }]))
   },
   6: {

bb86451fa22c37f313daef82e4f23ef6fde3fb70
Aes Sedai <zrlovin@gmail.com>
2017-12-30 20:04:44 -0600

    Handle mobile tooltip detection

--- imports/ui/components/abilityLibraryIcon/abilityLibraryIcon.js
+++ imports/ui/components/abilityLibraryIcon/abilityLibraryIcon.js
@@ -1,8 +1,9 @@
-import { Meteor } from 'meteor/meteor';
+import { Session } from "meteor/session";
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
 import './abilityLibraryIcon.html';
+
 
 let tooltip;
 Template.abilityLibraryIcon.onCreated(function bodyOnCreated() {
@@ -11,20 +12,27 @@ Template.abilityLibraryIcon.onCreated(function bodyOnCreated() {
 
 Template.abilityLibraryIcon.rendered = function () {
   if (!Template.instance().data.hideTooltip) {
+    const vm = this;
+    vm.state.set('tooltipOpen', false);
     tooltip = tippy(Template.instance().$('.item-icon-container')[0],
         {
           html: Template.instance().$('.item-tooltip-content')[0],
           performance: true,
           animateFill: false,
-          distance: 5
+          distance: 5,
+          onHide: function() {
+            vm.state.set('tooltipOpen', false);
+          }
         })
   }
-}
+};
 
 Template.abilityLibraryIcon.onDestroyed(function () {
-  let tooltipInstance = Template.instance().$('.item-icon-container')[0];
-  if (tooltip && tooltipInstance.hasOwnProperty('_tippy')) {
-    tooltipInstance._tippy.destroy();
+  if (tooltip) {
+    const tooltipInstance = Template.instance().$('.item-icon-container')[0];
+    if (tooltipInstance && tooltipInstance.hasOwnProperty('_tippy')) {
+      tooltipInstance._tippy.destroy();
+    }
   }
 })
 
@@ -33,6 +41,27 @@ Template.abilityLibraryIcon.events({
 
     if ($('body').hasClass('targetting-item')) {
       return;
+    }
+
+    if(Session.get('tooltipInput') === 'touch') {
+      if (!Template.instance().data.hideTooltip) {
+        if (instance.state.get('tooltipOpen')) {
+          // close tooltip
+          let tooltipInstance = Template.instance().$('.item-icon-container')[0];
+          if (tooltipInstance) {
+            tooltipInstance._tippy.hide();
+            instance.state.set('tooltipOpen', false);
+          }
+        } else {
+          // open tooltip
+          let tooltipInstance = Template.instance().$('.item-icon-container')[0];
+          if (tooltipInstance) {
+            tooltipInstance._tippy.show();
+            instance.state.set('tooltipOpen', true);
+          }
+          return;
+        }
+      }
     }
 
     const primaryAction = instance.data.item.primaryAction;
@@ -45,4 +74,4 @@ Template.abilityLibraryIcon.events({
       primaryAction.method();
     }
   }
-})
+});

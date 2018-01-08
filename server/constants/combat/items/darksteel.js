65842c25c0177505c51b5776c665fc8713589e97
Nick Breslin <nickbreslin@gmail.com>
2018-01-08 08:57:25 -0800

    Darksteel (Cursed + Ada)

---
+++ server/constants/combat/items/darksteel.js
@@ -0,0 +1,299 @@
+const DARKSTEEL_MULTIPLIER = 1.1;
+
+
+export const DARKSTEEL_ITEMS = {
+  "darksteel_dagger": {
+    "id": "darksteel_dagger",
+    "icon": "darksteelDagger.svg",
+    "category": "combat",
+    "weaponType": "dagger",
+    "slot": "mainHand",
+    "name": "darksteel dagger",
+    "sellPrice": 1650,
+    "description": "Af poorly made dagger.",
+    "isWeapon": true,
+    "isEquippable": true,
+    "stats": {
+      "attack": 17.4 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 32.7 * DARKSTEEL_MULTIPLIER,
+      "attackSpeed": 1,
+      "accuracy": 75.5 * DARKSTEEL_MULTIPLIER
+    },
+    requiredEquip: [{
+      type: 'skill',
+      name: 'attack',
+      level: 100
+    }]
+  },
+  "darksteel_spear": {
+    "id": "darksteel_spear",
+    "icon": "darksteelSpear.svg",
+    "category": "combat",
+    "weaponType": "spear",
+    "slot": "mainHand",
+    "name": "darksteel spear",
+    "sellPrice": 3000,
+    "description": "Used to train defense.",
+    "isWeapon": true,
+    "isEquippable": true,
+    "stats": {
+      "attack": 66.7 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 83.5 * DARKSTEEL_MULTIPLIER,
+      "attackSpeed": 0.5,
+      "accuracy": 83.5 * DARKSTEEL_MULTIPLIER,
+      "defense": 32.7 * DARKSTEEL_MULTIPLIER
+    },
+    "extraStats": {
+      "attack": 20 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 25.1 * DARKSTEEL_MULTIPLIER,
+      "accuracy": 25.1 * DARKSTEEL_MULTIPLIER,
+      "defense": 9.8 * DARKSTEEL_MULTIPLIER
+    },
+    requiredEquip: [{
+      type: 'skill',
+      name: 'defense',
+      level: 100
+    }]
+  },
+  "darksteel_short_sword": {
+    "id": "darksteel_short_sword",
+    "icon": "darksteelShortSword.svg",
+    "category": "combat",
+    "weaponType": "shortSword",
+    "slot": "mainHand",
+    "name": "darksteel short sword",
+    "sellPrice": 3000,
+    "description": "A good balance between offense and defense",
+    "isWeapon": true,
+    "isEquippable": true,
+    "stats": {
+      "attack": 66.7 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 99.5 * DARKSTEEL_MULTIPLIER,
+      "attackSpeed": 0.7,
+      "accuracy": 99.5 * DARKSTEEL_MULTIPLIER
+    },
+    "extraStats": {
+      "attack": 20 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 29.8 * DARKSTEEL_MULTIPLIER,
+      "accuracy": 29.8 * DARKSTEEL_MULTIPLIER
+    },
+    requiredEquip: [{
+      type: 'skill',
+      name: 'attack',
+      level: 100
+    }]
+  },
+  "darksteel_scimitar": {
+    "id": "darksteel_scimitar",
+    "icon": "darksteelScimitar.svg",
+    "category": "combat",
+    "weaponType": "shortSword",
+    "slot": "mainHand",
+    "name": "darksteel scimitar",
+    "sellPrice": 3000,
+    "description": "A good balance between offense and defense",
+    "isWeapon": true,
+    "isEquippable": true,
+    "stats": {
+      "attack": 73.7 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 116.7 * DARKSTEEL_MULTIPLIER,
+      "attackSpeed": 0.7,
+      "accuracy": 99.5 * DARKSTEEL_MULTIPLIER
+    },
+    "extraStats": {
+      "attack": 22.1 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 35 * DARKSTEEL_MULTIPLIER,
+      "accuracy": 29.8 * DARKSTEEL_MULTIPLIER
+    },
+    requiredEquip: [{
+      type: 'skill',
+      name: 'attack',
+      level: 105
+    }]
+  },
+  "darksteel_long_sword": {
+    "id": "darksteel_long_sword",
+    "icon": "darksteelLongSword.svg",
+    "category": "combat",
+    "weaponType": "longSword",
+    "slot": "mainHand",
+    "name": "darksteel long sword",
+    "sellPrice": 3000,
+    "description": "A pure offensive weapon",
+    "isTwoHanded": true,
+    "isWeapon": true,
+    "isEquippable": true,
+    "stats": {
+      "attack": 116.7 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 200.5 * DARKSTEEL_MULTIPLIER,
+      "attackSpeed": 0.5,
+      "accuracy": 134.1 * DARKSTEEL_MULTIPLIER
+    },
+    "extraStats": {
+      "attack": 35 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 60.2 * DARKSTEEL_MULTIPLIER,
+      "accuracy": 40.2 * DARKSTEEL_MULTIPLIER
+    },
+    requiredEquip: [{
+      type: 'skill',
+      name: 'attack',
+      level: 100
+    }]
+  },
+  
+  "darksteel_battle_axe": {
+    "id": "darksteel_battle_axe",
+    "icon": "darksteelBattleAxe.svg",
+    "category": "combat",
+    "weaponType": "battleAxe",
+    "slot": "mainHand",
+    "name": "darksteel battle axe",
+    "sellPrice": 3000,
+    "description": "A slow pure offensive weapon.",
+    "isTwoHanded": true,
+    "isWeapon": true,
+    "isEquippable": true,
+    "stats": {
+      "attack": 83.5 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 300 * DARKSTEEL_MULTIPLIER,
+      "attackSpeed": 0.3,
+      "accuracy": 134.1 * DARKSTEEL_MULTIPLIER,
+      "criticalChance": 5
+    },
+    "extraStats": {
+      "attack": 25.1 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 90 * DARKSTEEL_MULTIPLIER
+    },
+    requiredEquip: [{
+      type: 'skill',
+      name: 'attack',
+      level: 100
+    }]
+  },
+  "darksteel_horned_helmet": {
+    "id": "darksteel_horned_helmet",
+    "icon": "darksteelHornedHelmet.svg",
+    "category": "combat",
+    "slot": "head",
+    "name": "darksteel horned helmet",
+    "sellPrice": 3000,
+    "description": "Protect your head",
+    "isEquippable": true,
+    "stats": {
+      "attack": 17.4 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 17.4 * DARKSTEEL_MULTIPLIER,
+      "accuracy": 17.4 * DARKSTEEL_MULTIPLIER
+    },
+    "extraStats": {
+      "attack": 5.2 * DARKSTEEL_MULTIPLIER,
+      "attackMax": 5.2 * DARKSTEEL_MULTIPLIER,
+      "accuracy": 5.2 * DARKSTEEL_MULTIPLIER
+    },
+    requiredEquip: [{
+      type: 'skill',
+      name: 'defense',
+      level: 100
+    }]
+  },
+  "darksteel_helmet": {
+    "id": "darksteel_helmet",
+    "icon": "darksteelHelmet.svg",
+    "category": "combat",
+    "slot": "head",
+    "name": "darksteel helmet",
+    "sellPrice": 3000,
+    "description": "Protect your head",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 17.4 * DARKSTEEL_MULTIPLIER,
+      "defense": 17.4 * DARKSTEEL_MULTIPLIER,
+      "armor": 83.5 * DARKSTEEL_MULTIPLIER
+    },
+    "extraStats": {
+      "healthMax": 5.2 * DARKSTEEL_MULTIPLIER,
+      "defense": 5.2 * DARKSTEEL_MULTIPLIER,
+      "armor": 25.1 * DARKSTEEL_MULTIPLIER
+    },
+    requiredEquip: [{
+      type: 'skill',
+      name: 'defense',
+      level: 100
+    }]
+  },
+  "darksteel_chest_plate": {
+    "id": "darksteel_chest_plate",
+    "icon": "darksteelChestPlate.svg",
+    "category": "combat",
+    "slot": "chest",
+    "name": "darksteel chestplate",
+    "sellPrice": 3000,
+    "description": "Protect your heart",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 17.4 * DARKSTEEL_MULTIPLIER,
+      "defense": 17.4 * DARKSTEEL_MULTIPLIER,
+      "armor": 83.5 * DARKSTEEL_MULTIPLIER
+    },
+    "extraStats": {
+      "healthMax": 5.2 * DARKSTEEL_MULTIPLIER,
+      "defense": 5.2 * DARKSTEEL_MULTIPLIER,
+      "armor": 25.1 * DARKSTEEL_MULTIPLIER
+    },
+    requiredEquip: [{
+      type: 'skill',
+      name: 'defense',
+      level: 100
+    }]
+  },
+  "darksteel_plate_legs": {
+    "id": "darksteel_plate_legs",
+    "icon": "darksteelPlateLegs.svg",
+    "category": "combat",
+    "slot": "legs",
+    "name": "darksteel platelegs",
+    "sellPrice": 3000,
+    "description": "Protect your legs",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 17.4 * DARKSTEEL_MULTIPLIER,
+      "defense": 17.4 * DARKSTEEL_MULTIPLIER,
+      "armor": 83.5 * DARKSTEEL_MULTIPLIER
+    },
+    "extraStats": {
+      "healthMax": 5.2 * DARKSTEEL_MULTIPLIER,
+      "defense": 5.2 * DARKSTEEL_MULTIPLIER,
+      "armor": 25.1 * DARKSTEEL_MULTIPLIER
+    },
+    requiredEquip: [{
+      type: 'skill',
+      name: 'defense',
+      level: 100
+    }]
+  },
+  "darksteel_shield": {
+    "id": "darksteel_shield",
+    "icon": "darksteelShield.svg",
+    "category": "combat",
+    "weaponType": "shield",
+    "slot": "offHand",
+    "name": "darksteel shield",
+    "sellPrice": 3000,
+    "description": "Provides large defense bonuses",
+    "isEquippable": true,
+    "stats": {
+      "healthMax": 32.7 * DARKSTEEL_MULTIPLIER,
+      "defense": 51.1 * DARKSTEEL_MULTIPLIER,
+      "armor": 167.6 * DARKSTEEL_MULTIPLIER
+    },
+    "extraStats": {
+      "healthMax": 9.8 * DARKSTEEL_MULTIPLIER,
+      "defense": 15.3 * DARKSTEEL_MULTIPLIER,
+      "armor": 50.3 * DARKSTEEL_MULTIPLIER
+    },
+    requiredEquip: [{
+      type: 'skill',
+      name: 'defense',
+      level: 100
+    }]
+  }
+}

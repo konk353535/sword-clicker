97fb599df0095fccd1e76aa4c1df0d6cceb158d9
Nick Breslin <nickbreslin@gmail.com>
2018-01-08 23:12:27 -0800

    Clean rounding

--- server/constants/combat/items/darksteel.js
+++ server/constants/combat/items/darksteel.js
@@ -9,15 +9,15 @@ export const DARKSTEEL_ITEMS = {
     "weaponType": "dagger",
     "slot": "mainHand",
     "name": "darksteel dagger",
-    "sellPrice": 1650 * DARKSTEEL_MULTIPLIER,
+    "sellPrice": Math.round(1650 * DARKSTEEL_MULTIPLIER),
     "description": "A poorly made dagger.",
     "isWeapon": true,
     "isEquippable": true,
     "stats": {
-      "attack": 17.4 * DARKSTEEL_MULTIPLIER,
-      "attackMax": 32.7 * DARKSTEEL_MULTIPLIER,
+      "attack": (17.4 * DARKSTEEL_MULTIPLIER).toFixed(1),
+      "attackMax": 32.7 * DARKSTEEL_MULTIPLIER.toFixed(1),
       "attackSpeed": 1,
-      "accuracy": 75.5 * DARKSTEEL_MULTIPLIER
+      "accuracy": 75.5 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     requiredEquip: [{
       type: 'skill',
@@ -32,22 +32,22 @@ export const DARKSTEEL_ITEMS = {
     "weaponType": "spear",
     "slot": "mainHand",
     "name": "darksteel spear",
-    "sellPrice": 3000 * DARKSTEEL_MULTIPLIER,
+    "sellPrice": Math.round(3000 * DARKSTEEL_MULTIPLIER,
     "description": "Used to train defense.",
     "isWeapon": true,
     "isEquippable": true,
     "stats": {
-      "attack": 66.7 * DARKSTEEL_MULTIPLIER,
-      "attackMax": 83.5 * DARKSTEEL_MULTIPLIER,
+      "attack": 66.7 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "attackMax": 83.5 * DARKSTEEL_MULTIPLIER.toFixed(1),
       "attackSpeed": 0.5,
-      "accuracy": 83.5 * DARKSTEEL_MULTIPLIER,
-      "defense": 32.7 * DARKSTEEL_MULTIPLIER
+      "accuracy": 83.5 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "defense": 32.7 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     "extraStats": {
-      "attack": 20 * DARKSTEEL_MULTIPLIER,
-      "attackMax": 25.1 * DARKSTEEL_MULTIPLIER,
-      "accuracy": 25.1 * DARKSTEEL_MULTIPLIER,
-      "defense": 9.8 * DARKSTEEL_MULTIPLIER
+      "attack": 20 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "attackMax": 25.1 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "accuracy": 25.1 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "defense": 9.8 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     requiredEquip: [{
       type: 'skill',
@@ -62,20 +62,20 @@ export const DARKSTEEL_ITEMS = {
     "weaponType": "shortSword",
     "slot": "mainHand",
     "name": "darksteel short sword",
-    "sellPrice": 3000 * DARKSTEEL_MULTIPLIER,
+    "sellPrice": Math.round(3000 * DARKSTEEL_MULTIPLIER),
     "description": "A good balance between offense and defense",
     "isWeapon": true,
     "isEquippable": true,
     "stats": {
-      "attack": 66.7 * DARKSTEEL_MULTIPLIER,
-      "attackMax": 99.5 * DARKSTEEL_MULTIPLIER,
+      "attack": 66.7 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "attackMax": 99.5 * DARKSTEEL_MULTIPLIER.toFixed(1),
       "attackSpeed": 0.7,
-      "accuracy": 99.5 * DARKSTEEL_MULTIPLIER
+      "accuracy": 99.5 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     "extraStats": {
-      "attack": 20 * DARKSTEEL_MULTIPLIER,
-      "attackMax": 29.8 * DARKSTEEL_MULTIPLIER,
-      "accuracy": 29.8 * DARKSTEEL_MULTIPLIER
+      "attack": 20 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "attackMax": 29.8 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "accuracy": 29.8 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     requiredEquip: [{
       type: 'skill',
@@ -90,21 +90,21 @@ export const DARKSTEEL_ITEMS = {
     "weaponType": "longSword",
     "slot": "mainHand",
     "name": "darksteel long sword",
-    "sellPrice": 3000 * DARKSTEEL_MULTIPLIER,
+    "sellPrice": Math.round(3000 * DARKSTEEL_MULTIPLIER,
     "description": "A pure offensive weapon",
     "isTwoHanded": true,
     "isWeapon": true,
     "isEquippable": true,
     "stats": {
-      "attack": 116.7 * DARKSTEEL_MULTIPLIER,
-      "attackMax": 200.5 * DARKSTEEL_MULTIPLIER,
+      "attack": 116.7 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "attackMax": 200.5 * DARKSTEEL_MULTIPLIER.toFixed(1),
       "attackSpeed": 0.5,
-      "accuracy": 134.1 * DARKSTEEL_MULTIPLIER
+      "accuracy": 134.1 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     "extraStats": {
-      "attack": 35 * DARKSTEEL_MULTIPLIER,
-      "attackMax": 60.2 * DARKSTEEL_MULTIPLIER,
-      "accuracy": 40.2 * DARKSTEEL_MULTIPLIER
+      "attack": 35 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "attackMax": 60.2 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "accuracy": 40.2 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     requiredEquip: [{
       type: 'skill',
@@ -120,21 +120,21 @@ export const DARKSTEEL_ITEMS = {
     "weaponType": "battleAxe",
     "slot": "mainHand",
     "name": "darksteel battle axe",
-    "sellPrice": 3000 * DARKSTEEL_MULTIPLIER,
+    "sellPrice": Math.round(3000 * DARKSTEEL_MULTIPLIER),
     "description": "A slow pure offensive weapon.",
     "isTwoHanded": true,
     "isWeapon": true,
     "isEquippable": true,
     "stats": {
-      "attack": 83.5 * DARKSTEEL_MULTIPLIER,
-      "attackMax": 300 * DARKSTEEL_MULTIPLIER,
+      "attack": 83.5 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "attackMax": 300 * DARKSTEEL_MULTIPLIER.toFixed(1),
       "attackSpeed": 0.3,
-      "accuracy": 134.1 * DARKSTEEL_MULTIPLIER,
+      "accuracy": 134.1 * DARKSTEEL_MULTIPLIER.toFixed(1),
       "criticalChance": 5
     },
     "extraStats": {
-      "attack": 25.1 * DARKSTEEL_MULTIPLIER,
-      "attackMax": 90 * DARKSTEEL_MULTIPLIER
+      "attack": 25.1 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "attackMax": 90 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     requiredEquip: [{
       type: 'skill',
@@ -149,18 +149,18 @@ export const DARKSTEEL_ITEMS = {
     "category": "combat",
     "slot": "head",
     "name": "darksteel helmet",
-    "sellPrice": 3000 * DARKSTEEL_MULTIPLIER,
+    "sellPrice": Math.round(3000 * DARKSTEEL_MULTIPLIER,
     "description": "Protect your head",
     "isEquippable": true,
     "stats": {
-      "healthMax": 17.4 * DARKSTEEL_MULTIPLIER,
-      "defense": 17.4 * DARKSTEEL_MULTIPLIER,
-      "armor": 83.5 * DARKSTEEL_MULTIPLIER
+      "healthMax": 17.4 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "defense": 17.4 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "armor": 83.5 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     "extraStats": {
-      "healthMax": 5.2 * DARKSTEEL_MULTIPLIER,
-      "defense": 5.2 * DARKSTEEL_MULTIPLIER,
-      "armor": 25.1 * DARKSTEEL_MULTIPLIER
+      "healthMax": 5.2 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "defense": 5.2 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "armor": 25.1 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     requiredEquip: [{
       type: 'skill',
@@ -174,18 +174,18 @@ export const DARKSTEEL_ITEMS = {
     "category": "combat",
     "slot": "chest",
     "name": "darksteel chestplate",
-    "sellPrice": 3000 * DARKSTEEL_MULTIPLIER,
+    "sellPrice": Math.round(3000 * DARKSTEEL_MULTIPLIER),
     "description": "Protect your heart",
     "isEquippable": true,
     "stats": {
-      "healthMax": 17.4 * DARKSTEEL_MULTIPLIER,
-      "defense": 17.4 * DARKSTEEL_MULTIPLIER,
-      "armor": 83.5 * DARKSTEEL_MULTIPLIER
+      "healthMax": 17.4 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "defense": 17.4 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "armor": 83.5 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     "extraStats": {
-      "healthMax": 5.2 * DARKSTEEL_MULTIPLIER,
-      "defense": 5.2 * DARKSTEEL_MULTIPLIER,
-      "armor": 25.1 * DARKSTEEL_MULTIPLIER
+      "healthMax": 5.2 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "defense": 5.2 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "armor": 25.1 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     requiredEquip: [{
       type: 'skill',
@@ -199,18 +199,18 @@ export const DARKSTEEL_ITEMS = {
     "category": "combat",
     "slot": "legs",
     "name": "darksteel platelegs",
-    "sellPrice": 3000 * DARKSTEEL_MULTIPLIER,
+    "sellPrice": Math.round(3000 * DARKSTEEL_MULTIPLIER),
     "description": "Protect your legs",
     "isEquippable": true,
     "stats": {
-      "healthMax": 17.4 * DARKSTEEL_MULTIPLIER,
-      "defense": 17.4 * DARKSTEEL_MULTIPLIER,
-      "armor": 83.5 * DARKSTEEL_MULTIPLIER
+      "healthMax": 17.4 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "defense": 17.4 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "armor": 83.5 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     "extraStats": {
-      "healthMax": 5.2 * DARKSTEEL_MULTIPLIER,
-      "defense": 5.2 * DARKSTEEL_MULTIPLIER,
-      "armor": 25.1 * DARKSTEEL_MULTIPLIER
+      "healthMax": 5.2 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "defense": 5.2 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "armor": 25.1 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     requiredEquip: [{
       type: 'skill',
@@ -225,18 +225,18 @@ export const DARKSTEEL_ITEMS = {
     "weaponType": "shield",
     "slot": "offHand",
     "name": "darksteel shield",
-    "sellPrice": 3000 * DARKSTEEL_MULTIPLIER,
+    "sellPrice": Math.round(3000 * DARKSTEEL_MULTIPLIER),
     "description": "Provides large defense bonuses",
     "isEquippable": true,
     "stats": {
-      "healthMax": 32.7 * DARKSTEEL_MULTIPLIER,
-      "defense": 51.1 * DARKSTEEL_MULTIPLIER,
-      "armor": 167.6 * DARKSTEEL_MULTIPLIER
+      "healthMax": 32.7 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "defense": 51.1 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "armor": 167.6 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     "extraStats": {
-      "healthMax": 9.8 * DARKSTEEL_MULTIPLIER,
-      "defense": 15.3 * DARKSTEEL_MULTIPLIER,
-      "armor": 50.3 * DARKSTEEL_MULTIPLIER
+      "healthMax": 9.8 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "defense": 15.3 * DARKSTEEL_MULTIPLIER.toFixed(1),
+      "armor": 50.3 * DARKSTEEL_MULTIPLIER.toFixed(1)
     },
     requiredEquip: [{
       type: 'skill',

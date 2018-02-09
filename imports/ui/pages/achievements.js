7087c6b0ac6295ace1f87ea7ab702baa69e6c17c
Nick Breslin <nickbreslin@gmail.com>
2018-02-09 15:16:08 -0800

    Achievements controller

--- imports/ui/pages/achievements.js
+++ imports/ui/pages/achievements.js
@@ -14,6 +14,10 @@ import './achievements.html';
 Template.achievementsPage.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
 
+  Meteor.call('achievements.fetch', (err, res) => {
+    this.state.set('achievements', res);
+  })
+
   Tracker.autorun(() => {
     const myUser = Users.findOne({ _id: Meteor.userId() });
     if (myUser) {
@@ -23,19 +27,9 @@ Template.achievementsPage.onCreated(function bodyOnCreated() {
         this.state.set('currentTab', 'pq');
       }
     }
-  });
-
-
-  
+  });  
 });
 
-Template.achievementsPage.onCreated(function bodyOnCreated() {
-  this.state = new ReactiveDict();
-
-  Meteor.call('achievements.fetch', (err, res) => {
-    this.state.set('achievements', res);
-  })
-});
 
 Template.achievementsPage.events({
   'click .btn-collect'(event, instance) {
@@ -57,3 +51,72 @@ Template.achievementsPage.helpers({
     return Template.instance().state.get('achievements');
   }
 })
+
+
+
+
+
+
+
+Template.magicPage.events({
+
+  'click .learn-now'(event, instance) {
+    Meteor.call('skills.learnSkill', 'astronomy');
+  },
+
+  'click .spellBookTabLink'(event, instance) {
+    if (instance.state.get('currentTab') !== 'spellBook') {
+      instance.state.set('currentTab', 'spellBook');
+      Meteor.call('users.setUiState', 'magicTab', 'spellBook');
+    }
+  },
+
+  'click .astronomyTabLink'(event, instance) {
+    if (instance.state.get('currentTab') !== 'astronomy') {
+      instance.state.set('currentTab', 'astronomy');
+      Meteor.call('users.setUiState', 'magicTab', 'astronomy');
+    }
+  },
+
+  'click .abilitiesTabLink'(event, instance) {
+    if (instance.state.get('currentTab') !== 'abilities') {
+      instance.state.set('currentTab', 'abilities');
+      Meteor.call('users.setUiState', 'magicTab', 'abilities');
+    }
+  },
+});
+
+Template.magicPage.helpers({
+
+  learnRequirements() {
+    return Template.instance().state.get('learnRequirements');
+  },
+
+  hasLearnRequirements() {
+    return Template.instance().state.get('hasLearnRequirements');
+  },
+
+  learnRequirementsMet() {
+    const instance = Template.instance();
+    return function (met) {
+      instance.state.set('hasLearnRequirements', met);
+    }
+  },
+
+  astronomySkill() {
+    return Skills.findOne({ type: 'astronomy' });
+  },
+
+  showAstronomyTab() {
+    return Template.instance().state.get('currentTab') === 'astronomy';
+  },
+
+  showSpellBookTab() {
+    return Template.instance().state.get('currentTab') === 'spellBook';
+  },
+
+  showAbilitiesTab() {
+    return Template.instance().state.get('currentTab') === 'abilities';
+  }
+
+});

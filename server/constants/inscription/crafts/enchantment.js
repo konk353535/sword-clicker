bb15c54552f448e6009aa48a232201df30d19704
Aes Sedai <zrlovin@gmail.com>
2018-03-01 19:36:03 -0600

    Add enchantment targets to recipe names

--- server/constants/inscription/crafts/enchantment.js
+++ server/constants/inscription/crafts/enchantment.js
@@ -27,7 +27,7 @@ export const ENCHANTMENT_CRAFTS = {
   enchantment_barkskin: {
     recipeFor: 'inscription',
     produces: 'enchantment_barkskin',
-    name: 'Barkskin Enchantment',
+    name: 'Barkskin Enchantment [Chest]',
     id: 'enchantment_barkskin',
     category: 'enchantment',
     timeToCraft: 1,
@@ -49,7 +49,7 @@ export const ENCHANTMENT_CRAFTS = {
   enchantment_flaming_blade: {
     recipeFor: 'inscription',
     produces: 'enchantment_flaming_blade',
-    name: 'Flaming Blade Enchantment',
+    name: 'Flaming Blade Enchantment [Mainhand]',
     id: 'enchantment_flaming_blade',
     category: 'enchantment',
     timeToCraft: 1,
@@ -71,7 +71,7 @@ export const ENCHANTMENT_CRAFTS = {
   enchantment_enchanted_blade: {
     recipeFor: 'inscription',
     produces: 'enchantment_enchanted_blade',
-    name: 'Enchanted Blade Enchantment',
+    name: 'Enchanted Blade Enchantment [Mainhand]',
     id: 'enchantment_enchanted_blade',
     category: 'enchantment',
     timeToCraft: 1,
@@ -93,7 +93,7 @@ export const ENCHANTMENT_CRAFTS = {
   enchantment_intimidate: {
     recipeFor: 'inscription',
     produces: 'enchantment_intimidate',
-    name: 'Intimidate Enchantment',
+    name: 'Intimidate Enchantment [Head]',
     id: 'enchantment_intimidate',
     category: 'enchantment',
     timeToCraft: 1,
@@ -115,7 +115,7 @@ export const ENCHANTMENT_CRAFTS = {
   enchantment_fox_skin: {
     recipeFor: 'inscription',
     produces: 'enchantment_fox_skin',
-    name: 'Fox Skin Enchantment',
+    name: 'Fox Skin Enchantment [Legs]',
     id: 'enchantment_fox_skin',
     category: 'enchantment',
     timeToCraft: 1,
@@ -137,7 +137,7 @@ export const ENCHANTMENT_CRAFTS = {
   enchantment_rhino_skin: {
     recipeFor: 'inscription',
     produces: 'enchantment_rhino_skin',
-    name: 'Rhino Skin Enchantment',
+    name: 'Rhino Skin Enchantment [Legs]',
     id: 'enchantment_rhino_skin',
     category: 'enchantment',
     timeToCraft: 1,

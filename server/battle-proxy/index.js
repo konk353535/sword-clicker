26e26cf8fd441c43d6e52a0b9c81d737d98f3bf7
Peter Souza <psouza4@gmail.com>
2018-11-21 13:30:23 -0700

    proper termination of rejected connection attempts, #2 (balancer rejections) and configurable logging (currently rejected connections are hidden)

--- server/battle-proxy/index.js
+++ server/battle-proxy/index.js
@@ -20,16 +20,15 @@ const proxy = httpProxy.createProxyServer({
 const proxyServer = http.createServer(function(req, res) {
   const url = req.url.split('?')[1];
   const queryData = parse(url);
-  const balancer = queryData.balancer;
-  if (!balancer) {
-    return false;
+  const targetServerId = ((queryData.balancer) ? consistentHash.getNode(queryData.balancer) : undefined);
+  const targetServerUrl = ((targetServerId) ? SERVERS[targetServerId] : undefined);
+  const denyConnection = (!queryData.balancer || !queryData.userId || queryData.userId === 'undefined' || queryData.userId === '' || queryData.userId === 'unknown');
+  const connectionText = ((denyConnection) ? '  BLOCKED: no user' : `ACCEPTED ${queryData.userName} (#${queryData.userId})`) + ' :: ';
+  const wantLog = !denyConnection; // change this to just 'true' if we want to display blocked connections
+  
+  if (wantLog) {
+    console.log(`Balancer - ${queryData.balancer} | Proxying HTTP to ${targetServerId}`);
   }
-  const targetServerId = consistentHash.getNode(balancer);
-  const targetServerUrl = SERVERS[targetServerId];
-  const missingUser = (!queryData.userId || queryData.userId === 'undefined' || queryData.userId === '' || queryData.userId === 'unknown');
-  const connectionText = ((missingUser) ? '  BLOCKED: no user' : `ACCEPTED ${queryData.userName} (#${queryData.userId})`) + ' :: ';
-
-  console.log(`Balancer - ${balancer} | Proxying HTTP to ${targetServerId}`);
   
   let ipAddr = 'unknown';
   
@@ -58,7 +57,7 @@ const proxyServer = http.createServer(function(req, res) {
     console.log(`    ${connectionText}${ipAddr}`);
   }
   
-  if (!queryData.userId || queryData.userId === 'undefined' || queryData.userId === '' || queryData.userId === 'unknown') {
+  if (denyConnection) {
     try {
       res.writeHead(403, {'Content-Type': 'text/plain'});
       res.write("Unauthorized.");
@@ -68,7 +67,7 @@ const proxyServer = http.createServer(function(req, res) {
     return false;
   }
   
-  // slip the non-proxied original IP address of the player into the request URL so the battle node understands what IPs belong to what users
+  // slip the non-proxied original IP address of the player into the request URL so the battle node understands what IPs belong to which user
   req.url = `${req.url}&ipAddr=${ipAddr}`;
 
   proxy.web(req, res, { target: targetServerUrl });
@@ -77,45 +76,46 @@ const proxyServer = http.createServer(function(req, res) {
 proxyServer.on('upgrade', function (req, socket, head) {
   const url = req.url.split('?')[1];
   const queryData = parse(url);
-  const balancer = queryData.balancer;
-  if (!balancer) {
-    return false;
+  const targetServerId = ((queryData.balancer) ? consistentHash.getNode(queryData.balancer) : undefined);
+  const targetServerUrl = ((targetServerId) ? SERVERS[targetServerId] : undefined);
+  const denyConnection = (!queryData.balancer || !queryData.userId || queryData.userId === 'undefined' || queryData.userId === '' || queryData.userId === 'unknown');
+  const connectionText = ((denyConnection) ? '  BLOCKED: no user' : `ACCEPTED ${queryData.userName} (#${queryData.userId})`) + ' :: ';
+  const wantLog = !denyConnection; // change this to just 'true' if we want to display blocked connections
+  
+  if (wantLog) {
+    console.log(`Balancer - ${queryData.balancer} | Proxying WebS to ${targetServerId}`);
   }
-  const targetServerId = consistentHash.getNode(balancer);
-  const targetServerUrl = SERVERS[targetServerId];
-  const missingUser = (!queryData.userId || queryData.userId === 'undefined' || queryData.userId === '' || queryData.userId === 'unknown');
-  const connectionText = ((missingUser) ? '  BLOCKED: no user' : `ACCEPTED ${queryData.userName} (#${queryData.userId})`) + ' :: ';
-
-  console.log(`Balancer - ${balancer} | Proxying WebS to ${targetServerId}`);
 
   let ipAddr = 'unknown';
-  
-  try {
-    ipAddr = req.connection.remoteAddress;
-    let proxiedAddr = req.headers['x-forwarded-for'];
-    if (proxiedAddr === undefined || proxiedAddr.length === 0) {
-      proxiedAddr = req.headers['cf-connecting-ip'];
+
+  if (!denyConnection && wantLog) {
+    try {
+      ipAddr = req.connection.remoteAddress;
+      let proxiedAddr = req.headers['x-forwarded-for'];
+      if (proxiedAddr === undefined || proxiedAddr.length === 0) {
+        proxiedAddr = req.headers['cf-connecting-ip'];
+      }
+      if (proxiedAddr === undefined || proxiedAddr.length === 0) {
+        proxiedAddr = 'unknown';
+      }
+      if (proxiedAddr !== 'unknown') {
+        ipAddr = `${proxiedAddr} via ${ipAddr}`;
+      }
+      let cloudflareRay = req.headers['cf-ray'] ? req.headers['cf-ray'] : 'unknown';
+      let cloudflareCountry = req.headers['cf-ipcountry'] ? req.headers['cf-ipcountry'] : 'unknown';
+      
+      if (cloudflareRay === 'unknown') {
+        console.log(`    ${connectionText}${ipAddr}`);
+      } else {
+        console.log(`    ${connectionText}${ipAddr} routed @ ${cloudflareCountry}@${cloudflareRay}`);
+      }
     }
-    if (proxiedAddr === undefined || proxiedAddr.length === 0) {
-      proxiedAddr = 'unknown';
+    catch (err) {
+        console.log(`    ${connectionText}${ipAddr}`);
     }
-    if (proxiedAddr !== 'unknown') {
-      ipAddr = `${proxiedAddr} via ${ipAddr}`;
-    }
-    let cloudflareRay = req.headers['cf-ray'] ? req.headers['cf-ray'] : 'unknown';
-    let cloudflareCountry = req.headers['cf-ipcountry'] ? req.headers['cf-ipcountry'] : 'unknown';
-    
-    if (cloudflareRay === 'unknown') {
-      console.log(`    ${connectionText}${ipAddr}`);
-    } else {
-      console.log(`    ${connectionText}${ipAddr} routed @ ${cloudflareCountry}@${cloudflareRay}`);
-    }
-  }
-  catch (err) {
-      console.log(`    ${connectionText}${ipAddr}`);
   }
 
-  if (!queryData.userId || queryData.userId === 'undefined' || queryData.userId === '' || queryData.userId === 'unknown') {
+  if (denyConnection) {
     try {
       socket.close()
     } catch (err) {
@@ -123,7 +123,7 @@ proxyServer.on('upgrade', function (req, socket, head) {
     return false;
   }
   
-  // slip the non-proxied original IP address of the player into the request URL so the battle node understands what IPs belong to what users
+  // slip the non-proxied original IP address of the player into the request URL so the battle node understands what IPs belong to which user
   req.url = `${req.url}&ipAddr=${ipAddr}`;
   
   proxy.ws(req, socket, head, { target: targetServerUrl.replace('https','http') });

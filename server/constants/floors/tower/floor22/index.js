f5cd5da36aa0fbd6becaf6d7e8578705b7a60d6e
Peter Souza <psouza4@gmail.com>
2019-01-29 12:58:16 -0700

    add Lunar New Year event (2019, year of the pig) items to floor 22 loot table"

--- server/constants/floors/tower/floor22/index.js
+++ server/constants/floors/tower/floor22/index.js
@@ -30,6 +30,51 @@ export const TOWER_FLOOR_22 = {
         { type: 'item', itemId: 'radiant_shield_scroll', amount: 1 },
         { type: 'item', itemId: 'radiant_short_sword_scroll', amount: 1 }
       ]
+    }, {
+      chance: 1 / 64,
+      rewards: [
+        { type: 'item', itemId: 'event_lny_lion_claws', amount: 1 }
+      ]
+    }, {
+      chance: 1 / 64,
+      rewards: [
+        { type: 'item', itemId: 'event_lny_lion_body', amount: 1 }
+      ]
+    }, {
+      chance: 1 / 64,
+      rewards: [
+        { type: 'item', itemId: 'event_lny_lion_head', amount: 1 }
+      ]
+    }, {
+      chance: 1 / 64,
+      rewards: [
+        { type: 'item', itemId: 'event_lny_lunar_shield', amount: 1 }
+      ]
+    }, {
+      chance: 1 / 72,
+      rewards: [
+        { type: 'item', itemId: 'lny_pig_tome_level_1', amount: 1 }
+      ]
+    }, {
+      chance: 1 / 96,
+      rewards: [
+        { type: 'item', itemId: 'lny_pig_tome_level_2', amount: 1 }
+      ]
+    }, {
+      chance: 1 / 120,
+      rewards: [
+        { type: 'item', itemId: 'lny_pig_tome_level_3', amount: 1 }
+      ]
+    }, {
+      chance: 1 / 144,
+      rewards: [
+        { type: 'item', itemId: 'lny_pig_tome_level_4', amount: 1 }
+      ]
+    }, {
+      chance: 1 / 168,
+      rewards: [
+        { type: 'item', itemId: 'lny_pig_tome_level_5', amount: 1 }
+      ]
     }]))
   },
   4: {
@@ -41,6 +86,11 @@ export const TOWER_FLOOR_22 = {
         { type: 'item', itemId: 'radiant_helmet_scroll', amount: 1 },
         { type: 'item', itemId: 'radiant_chest_plate_scroll', amount: 1 }
       ]
+    }, {
+      chance: 1 / 64,
+      rewards: [
+        { type: 'item', itemId: 'gift_box_red_envelope', amount: 1 }
+      ]
     }]))
   },
   5: {
@@ -51,12 +101,17 @@ export const TOWER_FLOOR_22 = {
       rewards: [
         { type: 'item', itemId: 'radiant_kite_shield', amount: 1 }
       ]
-    },{
+    }, {
       chance: 1 / 16,
       rewards: [
         { type: 'item', itemId: 'radiant_axe_scroll', amount: 1 },
         { type: 'item', itemId: 'radiant_pickaxe_scroll', amount: 1 },
         { type: 'item', itemId: 'radiant_mining_anvil_scroll', amount: 1 }
+      ]
+    }, {
+      chance: 1 / 32,
+      rewards: [
+        { type: 'item', itemId: 'gift_box_red_envelope', amount: 1 }
       ]
     }]))
   },
@@ -69,6 +124,11 @@ export const TOWER_FLOOR_22 = {
         { type: 'item', itemId: 'radiant_spear_scroll', amount: 1 },
         { type: 'item', itemId: 'radiant_plate_legs_scroll', amount: 1 }
       ]
+    }, {
+      chance: 1 / 16,
+      rewards: [
+        { type: 'item', itemId: 'gift_box_red_envelope', amount: 1 }
+      ]
     }]))
   },
   7: {
@@ -79,6 +139,11 @@ export const TOWER_FLOOR_22 = {
       rewards: [
         { type: 'item', itemId: 'radiant_long_sword_scroll', amount: 1 },
         { type: 'item', itemId: 'radiant_battle_axe_scroll', amount: 1 }
+      ]
+    }, {
+      chance: 1 / 8,
+      rewards: [
+        { type: 'item', itemId: 'gift_box_red_envelope', amount: 1 }
       ]
     }]))
   },

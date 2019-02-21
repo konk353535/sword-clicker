a1970347a17db9b0826293146ce792858df04582
Peter Souza <psouza4@gmail.com>
2019-02-21 09:49:39 -0700

    refactor

--- server/api/state/state.js
+++ server/api/state/state.js
@@ -1,7 +1,7 @@
-import { State } from '/imports/api/state/state';
-import { STATE_BUFFS } from '/imports/constants/state';
 import { Meteor } from "meteor/meteor";
 
-Meteor.publish('state', function() {
-  return State.find({});
+import { State } from '/imports/api/state/state';
+
+Meteor.publish('state', function () {
+  return State.find();
 });

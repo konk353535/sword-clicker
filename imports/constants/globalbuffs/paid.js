54b5eaf6730a7141b5a019f1a1d2515aaa3a7957
Peter Souza <psouza4@gmail.com>
2019-02-21 09:48:16 -0700

    duplicate paid buffs for legacy IDs

--- imports/constants/globalbuffs/paid.js
+++ imports/constants/globalbuffs/paid.js
@@ -16,6 +16,22 @@ export const PAID_GLOBALBUFFS = {
     },
   },
 
+  buffCrafting: {
+    globalBuffId: 'buffCrafting',
+    name: function() {
+      return 'Global crafting buff';
+    },
+    icon: 'crafting',
+    extraDescription: 'This global buff can be purchased with bought gems in the shop.',
+    effects: function() {
+      return [
+        '+35% Crafting XP',
+        '+35% Inscription XP',
+        '+100% Gem Drop Rate XP',
+      ];
+    },
+  },
+
   paid_combat: {
     globalBuffId: 'paid_combat',
     dataBuffId: 'buffCombat', // legacy ID for this buff
@@ -33,9 +49,41 @@ export const PAID_GLOBALBUFFS = {
     },
   },
 
+  buffCombat: {
+    globalBuffId: 'buffCombat',
+    name: function() {
+      return 'Global combat buff';
+    },
+    icon: 'combat',
+    extraDescription: 'This global buff can be purchased with bought gems in the shop.',
+    effects: function() {
+      return [
+        '+20% Combat XP',
+        '+50% Drop Chance',
+        '+35% Astronomy XP',
+      ];
+    },
+  },
+
   paid_gathering: {
     globalBuffId: 'paid_gathering',
     dataBuffId: 'buffGathering', // legacy ID for this buff
+    name: function() {
+      return 'Global gathering buff';
+    },
+    icon: 'gathering',
+    extraDescription: 'This global buff can be purchased with bought gems in the shop.',
+    effects: function() {
+      return [
+        '+35% Mining XP',
+        '+35% Woodcutting XP',
+        '+35% Farming XP',
+      ];
+    },
+  },
+
+  buffGathering: {
+    globalBuffId: 'buffGathering',
     name: function() {
       return 'Global gathering buff';
     },

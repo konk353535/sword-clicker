3fff77b00b7e331b99a18d83e89692ad10cf3e73
Peter Souza <psouza4@gmail.com>
2019-02-20 23:59:24 -0700

    don't filter global buffs in state collection

--- server/api/state/state.js
+++ server/api/state/state.js
@@ -3,11 +3,5 @@ import { STATE_BUFFS } from '/imports/constants/state';
 import { Meteor } from "meteor/meteor";
 
 Meteor.publish('state', function() {
-  toPublish = [...Object.values(STATE_BUFFS)];
-
-  return State.find({
-    name: {
-      $in: toPublish
-    }
-  });
+  return State.find({});
 });

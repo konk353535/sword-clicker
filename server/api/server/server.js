6e773e4c2d1d16aca11e483e27d9421f45638e0b
Peter Souza <psouza4@gmail.com>
2019-03-03 10:13:14 -0700

    update comment

--- server/api/server/server.js
+++ server/api/server/server.js
@@ -5,7 +5,7 @@ import { Servers } from '/imports/api/servers/servers';
 import { Users } from '/imports/api/users/users.js';
 
 Meteor.methods({
-  // note: this isn't used by anything (directly, although an admin using the browser's dev console could call this)
+  // note: this isn't used by anything directly (although an admin using the 'dev' tab or a browser's dev console could call this)
   'server.info'() {
     const userDoc = Users.findOne({ _id: Meteor.userId() });
     const userIsAdmin = userDoc && userDoc.isSuperMod;

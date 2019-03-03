6c957b79269f069dff20d421e42b8058525b4de2
Peter Souza <psouza4@gmail.com>
2019-03-03 14:44:28 -0700

    complete rewrite for number formatter and add full comments

--- imports/ui/components/formatNumber/formatNumber.js
+++ imports/ui/components/formatNumber/formatNumber.js
@@ -5,7 +5,7 @@ import Numeral from 'numeral';
 import { Users } from '/imports/api/users/users.js';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
-import { CDbl, CInt, autoPrecisionValue } from '/imports/utils';
+import { IsValid, CInt, CDbl, True, False, autoPrecisionValue } from '/imports/utils';
 
 import './formatNumber.html';
 
@@ -28,46 +28,67 @@ Template.formatNumber.helpers({
   formattedNumber() {
     const instance = Template.instance();
     const templateData = instance.data;
-    let number = CDbl(templateData.number);
-    const decimalPlaces = templateData.decimal || 0;
-    const wantDecimal = !templateData.noDecimal;
-    const wantShorthand = templateData.forceShorthand || instance.state.get('showNumberShorthand');
-    const wantBlankZero = templateData.blankZero;
-    const wantAutoPrecision = (typeof templateData.autoPrecision === 'undefined') ? true : templateData.autoPrecision; // true by default
+
+    let number = CDbl(templateData.number); // 0.0 by default
+
+    const options = { };
     
-    if (wantBlankZero) {
-      if (CInt(number) === 0) {
-        return '';
-      }
+    options.specifiedDecimalPrecision = IsValid(templateData.decimal) && (typeof templateData.decimal === 'number'); // false by default (unless '.decimal' is explicitly set to a number)
+    options.forcedNoDecimal = False(templateData.noDecimal); // false by default (unless '.noDecimal' is explicitly set)
+    options.wantShorthand = False(instance.state.get('showNumberShorthand')) || False(templateData.forceShorthand); // false by default (for both values, thus this whole value, unless 'forceShorthand' is explicitly set or user's state 'showNumberShorthand' option is enabled)
+    options.wantBlankZero = False(templateData.blankZero); // false by default (unless '.blankZero' is explicitly set)
+
+    options.decimalPrecision = (options.specifiedDecimalPrecision) ? CInt(templateData.decimal) : 1; // 1 by default
+    options.haveDecimalPrecision = (!options.forcedNoDecimal) && (options.decimalPrecision > 0); // true by default (unless '.decimal' or '.noDecimal' is explicitly set)
+    options.wantForcedDecimal = (options.haveDecimalPrecision) && (options.specifiedDecimalPrecision); // false by default (unless '.decimal', '.noDecimal', or '.decimal' is explicitly set)
+    options.wantAutoPrecision = True(templateData.autoPrecision); // true by default
+
+    //console.log('Formatting number', number);
+    
+    if (!options.haveDecimalPrecision) {
+      number = Math.floor(number);
+    } else if (options.wantAutoPrecision) {
+      number = autoPrecisionValue(number);
+    }
+
+    options.showDecimal = options.wantForcedDecimal || (options.haveDecimalPrecision && (Math.floor(number) != number));
+
+    //console.log(options);
+    //console.log('Formatted number', number);
+
+    // Stop here and return a blank value if the number is 0 and '.blankZero' was explicitly set
+    if ((options.wantBlankZero) && (number == 0)) {
+      return '';
     }
     
-    if (wantAutoPrecision) {
-      number = autoPrecisionValue(number)
-      
-      if (wantShorthand && number >= 1000) {
+    // NOTE: for numbers < 1,000, if we're forcing precision to a specific decimal place, use a simple number formatter
+    
+    if ((options.wantForcedDecimal) && (number < 1000)) {
+      return number.toFixed(options.decimalPrecision);
+    }
+    
+    // NOTE: for numbers >= 1,000, 'specifiedDecimalPrecision' is ignored and all decimal precision will be displayed which
+    //       means wantAutoPrecision should be on by default to cap this for large numbers (although this is NOT enforced).
+
+    // If we want shorthand numbers...
+    if (options.wantShorthand) {
+      // ... and are forcing decimal precision -- or -- the number >= 1,000 (required so 1,200 will appear as 1.2k in shorthand, etc.)
+      if (options.wantForcedDecimal || number >= 1000) {
+        // ...... display  as shorthand (including any decimals in the number)
         return Numeral(number).format('0.0a');
       }
       
-      return number.toLocaleString();
+      // ... otherwise, display as shorthand (but don't display decimals)
+      return Numeral(number).format('0a');
     }
 
-    
-    if (number < 1000) {
-      if (decimalPlaces) {
-        return number.toFixed(CInt(decimalPlaces));
-      }
-      
-      return Math.floor(number);
+    // If we are forcing decimal precision -- or -- we have not specified to disable decimals and we have decimal precision in the number
+    if (options.showDecimal) {
+      // ... display as formatted xx,xxx,xxx.yyy (including any decimals in the number)
+      return Numeral(number).format('0,0.0');
     }
 
-    if (wantShorthand && number >= 1000) {
-      if (!wantDecimal) {
-        return Numeral(number).format('0a');
-      }
-
-      return Numeral(number).format('0.0a');
-    }
-    
+    // Otherwise, display as formatted xx,xxx,xxx (but don't display decimals)
     return Numeral(number).format('0,0');
   }
 });

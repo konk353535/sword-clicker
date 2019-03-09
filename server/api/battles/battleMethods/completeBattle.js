7fd6f819a3543c31121aa983fbb46515049297a8
Peter Souza <psouza4@gmail.com>
2019-03-09 11:05:39 -0700

    debug

--- server/api/battles/battleMethods/completeBattle.js
+++ server/api/battles/battleMethods/completeBattle.js
@@ -496,6 +496,8 @@ const battleHandler_RecordBossDamage = function(actualBattle) {
 
 export const completeBattle = function(actualBattle) {
   const finalTickEvents = [];
+
+  console.log(" ####  completeBattle()  #### ");
   
   let hadCompanions = false;
   actualBattle.units.forEach((unit, idx) => {
@@ -505,8 +507,6 @@ export const completeBattle = function(actualBattle) {
     }
   });
 
-  /*
-  console.log(" ####  completeBattle()  #### ");
   console.log(`Bonus loot: ${actualBattle.bonusLoot}`);
   console.log(`Extra loot table:`, actualBattle.extraLootTable);
   actualBattle.completedEnemies.forEach((enemy) => {
@@ -525,8 +525,6 @@ export const completeBattle = function(actualBattle) {
   });
   console.log("Had companions?", hadCompanions);
   console.log(actualBattle);
-  console.log(" ####  completeBattle()  #### ");
-  */
   
   // chance for companion tokens is 0%
   let chanceForTokens = 0;
@@ -557,6 +555,7 @@ export const completeBattle = function(actualBattle) {
   const battlesDeleted = removeBattle(actualBattle.id)
 
   if (battlesDeleted <= 0) {
+    console.log(" ####  completeBattle() early exit  #### ");
     return;
   }
 
@@ -1239,6 +1238,8 @@ export const completeBattle = function(actualBattle) {
 
   delete actualBattle; // javascript 'delete' keyword does nothing on variables, it only unsets properties on an object (try it in console)
                        // see https://www.w3schools.com/js/js_object_properties.asp
+
+  console.log(" ####  completeBattle()  #### ");
 };
 
 JsonRoutes.add("post", "/methods/completeBattle", function (req, res, next) {
